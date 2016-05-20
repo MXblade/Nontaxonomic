4cdaa431f24260bae53583afaebcb110eb69e089
M.X <chinamxcode@gmail.com>
2016-05-20 21:07:02 +0800

    添加包注释

--- src/main/java/bistu/idcc/exam/package-info.java
+++ src/main/java/bistu/idcc/exam/package-info.java
@@ -1,5 +1,8 @@
 /**
- * 
+ * 进行第一次实验的训练测试
+ * Split_feature 对多个特征的文件进行单特征拆分，需给定输入文件路径和输出文件夹路径
+ * Split_train 将数据分为训练语料和测试语料， 给定文件路径inpath和训练语料的数目num
+ * Test_main 调用svm测试结果
  */
 /**
  * @author Joen
